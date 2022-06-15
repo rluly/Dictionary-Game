3b3ea9204098fc09154b39ed2406b04d5868d000
rluly <ryanluly1@gmail.com>
2022-06-15 16:10:25 -0700

    MySQL compatibility

--- index.py
+++ index.py
@@ -1,5 +1,7 @@
 import random
 from datetime import datetime
+import mysql.connector
+from mysql.connector import errorcode
 
 f = open("dictionary.txt","r")
 list = []
@@ -15,6 +17,17 @@ win = 0
 print("Welcome to the Dictionary Game. There is a hidden, common word and you must submit guesses to try and find it. Please log in or sign up via username and password to save highscores and fastest times.")
 username = input("Please input a username: ")
 password = input("Please input a password: ")
+
+try:
+    cnx = mysql.connector.connect(user="rluly", password='barely67', host='localhost', database = 'groomstone')
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Connection failed")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print("Connection successful")
+
 while(not win):
     choice = input("Choose a word: ")
     # print(choice)
@@ -25,3 +38,5 @@ while(not win):
         print("The answer is closer to A.")
     else:
         print("The answer is closer to Z.")
+
+cnx.close()
